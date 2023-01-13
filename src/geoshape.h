f4e5eec70cd350c3dc88873a4b5a3834352b8d26
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-01-13 22:01:13 +0800

    Create improved system for formulas

---
+++ src/geoshape.h
@@ -0,0 +1,16 @@
+#ifndef GEOSHAPE_H_
+#define GEOSHAPE_H_
+
+#include "./shape.h"
+
+void set_square(Square *_square, float n_side);
+void set_rectangle(Rectangle *_rect, float n_len, float n_wid);
+void set_circle(Circle *_circle, float n_rad);
+void set_triangle(Triangle *_triangle, float n_base, float n_height);
+
+float square(Square *_square);
+float rectangle(Rectangle *_rectangle);
+float circle(Circle *_circle);
+float triangle(Triangle *_triangle);
+
+#endif // GEOSHAPE_H_
