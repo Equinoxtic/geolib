b9ad6bb5edc0cba0b17f0ac85d9dd5c2efdf0e7f
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-01-14 13:43:35 +0800

    Updated testing

---
+++ src/testutils.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void print_test_passed(const char* s_test)
+{
+	printf("(*) - Test Passed: %s\n", s_test);
+}
+
+void print_all_test_passed()
+{
+	printf("\n> All tests passed!\n");
+}
--- test.c
+++ test.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+#include "./src/shape.h"
 #include "./src/geomath.h"
+#include "./src/geoshape.h"
 #include "./src/geocore.h"
 #include "./src/geosimpcore.h"
 #include "./src/sleep.h"
+#include "./src/testutils.h"
 
 void multi_operation_test()
 {
@@ -20,7 +24,30 @@ void geom_formulas_test()
 	printf("\n[Square]: %g", area_square(12));
 	printf("\n[Rectangle]: %g", area_rect(12, 24));
 	printf("\n[Circle]: %g", area_circle(16));
-	printf("\n[Triangle]: %g\n", area_triangle(13, 18));
+	printf("\n[Triangle]: %g", area_triangle(13, 18));
+	printf("\n[Trapezoid]: %g\n", area_trapezoid(24, 12));
+}
+
+void geoshapes_test()
+{
+	Square sqr;
+	Rectangle rect;
+	Circle circ;
+	Triangle trig;
+	Trapezoid trpz;
+
+	set_square(&sqr, 4);
+	set_rectangle(&rect, 6, 3);
+	set_circle(&circ, 7);
+	set_triangle(&trig, 8, 4);
+	set_trapezoid(&trpz, 4, 10);
+
+	printf("\n<GeoShapes test>\n");
+	printf("\n[Square]: %g", square(&sqr));
+	printf("\n[Rectangle]: %g", rectangle(&rect));
+	printf("\n[Circle]: %g", circle(&circ));
+	printf("\n[Triangle]: %g", triangle(&trig));
+	printf("\n[Trapezoid]: %g\n", trapezoid(&trpz));
 }
 
 void sleep_stress_test()
@@ -47,15 +74,46 @@ void neg_and_pos_test(float val)
 	}
 }
 
+void geomformula_plus_test()
+{
+	printf("\n<Geometric Formulas+ Test>\n");
+	printf("\n[Perimeter (Square)]: %g", perim_square(23));
+	printf("\n[Perimeter (Rectangle)]: %g", perim_rect(14, 26));
+	printf("\n[Circle Circumference]: %g\n", circ_circle(35));
+}
+
+void geoshapes_plus_test()
+{
+	Square sqr;
+	Rectangle rect;
+	Circle circ;
+	
+	set_square(&sqr, 18);
+	set_rectangle(&rect, 12, 32);
+	set_circle(&circ, 24);
+	
+	printf("\n<GeoShapes+ Test>\n");
+	printf("\n[Perimeter (Square)]: %g", square_perim(&sqr));
+	printf("\n[Perimeter (Rectangle)]: %g", rectangle_perim(&rect));
+	printf("\n[Circle Circumference]: %g\n", circle_circ(&circ));
+}
+
 int main(int argc, char **argv)
 {
+	unsigned def_ms = 1750;
+
 	printf("\n> Running tests...\n");
 	
-	multi_operation_test();
-	geom_formulas_test();
-	neg_and_pos_test(2.3f);
-	sleep_stress_test();
-	
+	multi_operation_test();		print_test_passed("Multi-Operation-Test");				nsleep(def_ms);
+	geom_formulas_test();		print_test_passed("Geometric Formulas");				nsleep(def_ms);
+	geoshapes_test();			print_test_passed("GeoShapes Test");					nsleep(def_ms);
+	neg_and_pos_test(2.3f);		print_test_passed("Negative and Positive outputs");		nsleep(def_ms);
+	sleep_stress_test();		print_test_passed("Sleep Stress");						nsleep(def_ms);
+	geomformula_plus_test();	print_test_passed("Geometric Formulas+");				nsleep(def_ms);
+	geoshapes_plus_test();		print_test_passed("GeoShapes+");						nsleep(def_ms);
+
+	print_all_test_passed();
+
 	return 0;
 }
 
