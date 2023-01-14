84e9d8bee40d4e108af16e9f93a5fde67f14d038
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-01-14 16:21:52 +0800

    Create string library

---
+++ src/stringlib.c
@@ -0,0 +1,25 @@
+#include<stdbool.h>
+#include<ctype.h>
+#include<string.h>
+
+bool stringcompare(char *_a, char *_b)
+{
+	return (strcmp(_a, _b) == 0);
+}
+
+char* stringlower(char *_str)
+{
+	_str = strdup(_str);
+	for (char *p_str = _str; *p_str; ++p_str)
+		*p_str = toupper(*p_str);
+	return _str;
+}
+
+char* stringupper(char *_str)
+{
+	_str = strdup(_str);
+	for (char *p_str = _str; *p_str; ++p_str)
+		*p_str = tolower(*p_str);
+	return _str;
+}
+
---
+++ src/stringlib.h
@@ -0,0 +1,8 @@
+#ifndef STRINGLIB_H_
+#define STRINGLIB_H_
+
+bool stringcompare(char* _a, char* _b);
+char* stringlower(char* _str);
+char* stringupper(char* _str);
+
+#endif // STRINGLIB_H_
